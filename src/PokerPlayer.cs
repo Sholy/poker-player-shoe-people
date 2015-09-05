f9eb1e7a52e43897311c99654e6b0b0c2956e83b
Igor Dostanic <dostanik@gmail.com>
2015-09-05 11:12:56 +0200

    random

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -9,7 +9,8 @@ namespace Nancy.Simple
 		public static int BetRequest(JObject gameState)
 		{
 			//TODO: Use this method to return the value You want to bet
-			return 1;
+				Random r=new Random();
+			return r.next(10,20);
 		}
 
 		public static void ShowDown(JObject gameState)
